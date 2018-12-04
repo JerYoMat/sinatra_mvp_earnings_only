496a141c517fc546cb943fd7ea995ff82526fe70
Jeremy <jeremy.emata@gmail.com>
2018-12-03 22:47:45 -0500

    added slug to user class

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,9 +1,11 @@
 class LoansController < ApplicationController
 
   get '/loans' do
+    #check that the user is logged in - if yes show only their loans else redirect to login
+  
   end
 
-  get '/loans/new' do 
+  get '/loans/new' do
   end
 
   post '/loans' do
