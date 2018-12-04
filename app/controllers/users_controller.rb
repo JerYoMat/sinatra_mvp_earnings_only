519f8eef4430db74727a91d3094213dc71bac3ca
Jeremy <jeremy.emata@gmail.com>
2018-12-03 22:53:23 -0500

    added users/slug route to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,30 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
-  end
-
-  post '/signup' do
-  end
-
   get '/login' do
   end
 
   post '/login' do
   end
 
+
+  get '/users/:slug' do
+    if !logged_in?
+      redirect to '/login'
+    else
+      erb :'users/show'
+    end
+  end
+
+
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+
+
   get '/logout' do
-  end 
+  end
 
 end
