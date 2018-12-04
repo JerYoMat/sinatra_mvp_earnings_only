566cd8690251e6541a734e46cc8811203c36b03e
Jeremy <jeremy.emata@gmail.com>
2018-12-03 20:32:02 -0500

    added instance methods to loans and validated with one example.  order of operations currently handled in the ugliest way ever

--- app/models/loans.rb
+++ app/models/loans.rb
@@ -1,7 +1,33 @@
 class Loan < ActiveRecord::Base
   belongs_to :user
+=begin
+#for when in irb
 
+attr_accessor :loan_face_value, :loan_present_value, :loan_term, :annual_rate
+=end
   def periodic_rate
+    @annual_rate / 12
+  end
+
+  def monthly_payment
+    r = self.periodic_rate
+    pv = self.loan_present_value
+    n = self.loan_term
+
+    numerator = r * pv
+    rplusone = r + 1
+    exponent = n * -1
+    almostdenominator = rplusone ** exponent
+    denominator = 1 - almostdenominator
+    numerator / denominator
   end
 
 end
+=begin
+p = .87915887230009850
+l = Loan.new
+l.loan_face_value = 10
+l.loan_present_value = 10
+l.loan_term = 12
+l.annual_rate = 0.1
+=end
