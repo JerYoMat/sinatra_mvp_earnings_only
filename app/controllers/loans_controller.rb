9228cc5c2b0399141ddc79f8e1405fe8f5bf1136
Jeremy <jeremy.emata@gmail.com>
2018-12-04 09:14:02 -0500

    route for submitting create form - needs to be cleaned up but looks to work

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -4,7 +4,7 @@ class LoansController < ApplicationController
     if !logged_in?
       redirect to '/login'
     else
-      # still need the loans of that user to load. 
+      # still need the loans of that user to load.
       @loans = 4
       erb :'/loans/loans'
      end
@@ -12,10 +12,29 @@ class LoansController < ApplicationController
 
 
   get '/loans/new' do
-
+     if logged_in?
+       erb :'loans/create_loan'
+     else
+       redirect to '/login'
+     end
   end
 
   post '/loans' do
+    if logged_in?
+      if params[:loan_amount] != "" && params[:origination_fees] != "" && params[:loan_term] != "" && params[:annual_rate] != ""
+        @loan = current_user.loans.build(loan_face_value: params[:loan_amount], loan_present_value: params[:origination_fees], loan_term: params[:loan_term], annual_rate: params[:annual_rate])
+          if @loan.save
+            redirect to "/loans/#{@loan.id}"
+          else
+            redirect to '/loans/new'
+          end
+        else
+          redirect to '/loans/new'
+        end
+     else
+      redirect to '/login'
+    end
+
   end
 
   get '/loans/:id' do
