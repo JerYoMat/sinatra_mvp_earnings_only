1da8d34d169ecdfecfa7f137e06f0956ee5e020d
Jeremy <jeremy.emata@gmail.com>
2018-12-03 23:02:29 -0500

    DRY - added create from form to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   validates :username, :password, presence: true
   has_secure_password
 
+    def self.create_from_form(params)
+      User.create(:username => params[:username], :password => params[:password])
+    end
+
     def slug
       username.downcase.gsub(" ","-")
     end
