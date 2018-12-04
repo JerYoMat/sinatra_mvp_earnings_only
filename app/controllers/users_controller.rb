6c85119ff8f1abe1d773d05cc92679dbf3278e25
Jeremy <jeremy.emata@gmail.com>
2018-12-03 23:16:28 -0500

    changed user routes to have loan summary to be visible via users/username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
       if !logged_in?
         redirect to '/login'
       else
+        @loans = Loans.find_by(:user_id => current_user)
         erb :'users/show'
       end
     end
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect to "/loans"
+      redirect to "/users/:slug"
     else
       redirect to '/signup'
     end
@@ -44,12 +45,18 @@ class UsersController < ApplicationController
        @user = User.create_from_form(params)
        session[:user_id] = @user.id
        redirect to '/loans'
-     end 
+     end
   end
 
 
 
   get '/logout' do
+    if logged_in?
+      sessions.destroy
+      redirect to '/login'
+    else
+      redirect to '/signup'
+    end
   end
 
 end
