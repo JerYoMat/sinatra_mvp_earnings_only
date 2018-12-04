cab7920189965a3b16209d544a2b6528d5362591
Jeremy <jeremy.emata@gmail.com>
2018-12-03 19:59:25 -0500

    DRY - reuse helpers from fwitter for current user and logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,15 @@ class ApplicationController < Sinatra::Base
     set :session_secret, 'tbdsomethinglongandhardwhenitmatters'
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
+
 
 end
