a3d4f78243dc789e13814abbe468419cb7220fec
Jeremy <jeremy.emata@gmail.com>
2018-12-06 22:36:46 -0500

    trimmed user class down to reqs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,4 @@ class User < ActiveRecord::Base
       User.create(:username => params[:username], :password => params[:password])
     end
 
-    def slug
-      username.downcase.gsub(" ","-")
-    end
-
-    def self.find_by_slug(slug)
-      User.all.find{|user| user.slug == slug}
-    end
-
 end
